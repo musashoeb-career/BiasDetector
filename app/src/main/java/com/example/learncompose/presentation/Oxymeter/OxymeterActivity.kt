02e6e9c8039b272059e7c427783085f12e3a5d54
MusaShoeb <mshoeb@umich.edu>
2025-02-16 13:32:28 -0500

    Oxymeter Cleanup

--- app/src/main/java/com/example/learncompose/presentation/Oxymeter/OxymeterActivity.kt
+++ app/src/main/java/com/example/learncompose/presentation/Oxymeter/OxymeterActivity.kt
@@ -7,8 +7,12 @@ import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels // Use this to inject the ViewModel
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -17,6 +21,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
@@ -51,7 +56,7 @@ class OxymeterActivity : ComponentActivity() {
             val scope = rememberCoroutineScope()
             // Composable content
             WatchTheme {
-                WearApp(
+                OxymeterView(
                     onStartTestClick = {
                         scope.launch {
                             viewModel.startTest()                                   }
@@ -67,11 +72,36 @@ class OxymeterActivity : ComponentActivity() {
 }
 
 @Composable
-fun WearApp(onStartTestClick: () -> Unit, onStopTestClick: () -> Unit, sp02Level: Int) {
+fun OxymeterView(onStartTestClick: () -> Unit, onStopTestClick: () -> Unit, sp02Level: Int) {
 
-    Text(text = "Sp02 Level: $sp02Level")
 
-    Row {
+    Column (
+        modifier = Modifier
+            .fillMaxSize()  // Make the Column fill the entire screen
+            .background(WatchTheme.colorScheme.background)
+            .padding(top = 20.dp),
+        horizontalAlignment = Alignment.CenterHorizontally, // Align children horizontally in the center
+        verticalArrangement = Arrangement.Center // Align children vertically in the center
+    ){
+
+        Text(text = "Oxygen Level",
+            style = TextStyle(
+                fontFamily = Jura,
+                fontSize = 25.sp,
+                color = WatchTheme.colorScheme.primary
+            ))
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Text(text = "$sp02Level %",
+             style = TextStyle(
+                 fontFamily = Jura,
+                 fontSize = 40.sp,
+                 color = WatchTheme.colorScheme.primary
+             ))
+
+        Spacer(modifier = Modifier.height(16.dp))
+
         androidx.wear.compose.material.Button(
             onClick = {
                 onStartTestClick()
@@ -83,7 +113,7 @@ fun WearApp(onStartTestClick: () -> Unit, onStopTestClick: () -> Unit, sp02Level
             colors = ButtonDefaults.buttonColors(backgroundColor = WatchTheme.colorScheme.secondary)
         ) {
             Text(
-                "Start Test",
+                "Measure",
                 style = TextStyle(
                     fontFamily = Jura,
                     fontSize = 15.sp,
@@ -100,10 +130,10 @@ fun WearApp(onStartTestClick: () -> Unit, onStopTestClick: () -> Unit, sp02Level
                 .padding(2.dp)
                 .fillMaxWidth(0.4f)
                 .height(30.dp),
-            colors = ButtonDefaults.buttonColors(backgroundColor = WatchTheme.colorScheme.secondary)
+            colors = ButtonDefaults.buttonColors(backgroundColor = WatchTheme.colorScheme.tertiary)
         ) {
             Text(
-                "Stop Test",
+                "Continue",
                 style = TextStyle(
                     fontFamily = Jura,
                     fontSize = 15.sp,
